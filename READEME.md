d44cf0ba7095652a73d555fb3b8db298ee22a0a5
PDismore <pat.dismore@gmail.com>
2022-12-07 12:27:06 -0500

    db, schema, and seeds creation

---
+++ READEME.md
@@ -0,0 +1,5 @@
+# Employee Tracker
+# About
+This app allows you to tack employees across departments and roles, and allows you to add (and hopefully edit/ remove) employees as well as track salaries. It utilizes Node, Inquirer and MYSQL.
+
+# Video
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department
+  (department)
+  VALUES
+  ('Sales'),
+  ('HR'),
+  ('Engineering');
+
+  INSERT INTO role
+  (title, salary)
+  VALUES
+  ('Junior Engineer', 65000),
+  ('Senior Engineer' 80000),
+  ('Manager', 75000),
+  ('Employee'50000);
+
+  INSERT INTO employee
+  (first_name, last_name)
+  VALUES
+  ('Jim', 'Random'),
+  ('Suzy', 'LeRando'),
+  ('Phil', 'Aleatorio'),
+  ('Steve', 'Losowy'),
+  ('Samantha','Náhodný '),
+  ('John', 'Doe'),
+
+  
